8262743d0a5d027d48fe6e163e8d7310b2b05a8a
Ricky Nyairo <rickynyairo@gmail.com>
2018-08-27 11:40:42 +0300

    [Chore #160054467] Wrote tests for the logout functionality

--- tests/test_user.py
+++ tests/test_user.py
@@ -4,6 +4,7 @@ This module collects the tests for the normal user
 # standard imports
 import unittest
 from ..user import User
+from datetime import datetime
 
 class TestUser(unittest.TestCase):
     """This class collects the test for the regular user"""
@@ -19,3 +20,14 @@ class TestUser(unittest.TestCase):
         self.assertEqual(new_user.password, params['password'])
         # check that the new user does not have any comments
         self.assertEqual(0, len(new_user.comments))
+    
+    def test_user_logout(self):
+        """test that a logged in user can logout"""
+        params = {
+            'username':'userlogout',
+            'password':'password'
+        }
+        new_user = User(**params)
+        self.assertTrue(new_user.lastLoggedInAt < datetime.now())
+        self.assertEqual(new_user.logout(), 'logged out')
+        self.assertFalse(new_user.online)
